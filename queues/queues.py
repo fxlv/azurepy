39f8f5030f37989515e393dad110f31c982d7e31
Kaspars Mickevics <kaspars@fx.lv>
2015-02-27 22:32:33 +0200

    added queue_count(), get_queue_names() functions

--- queues/queues.py
+++ queues/queues.py
@@ -4,23 +4,35 @@ import account
 
 class Queue:
 
-    def __init__(self, name):
+    def __init__(self, name, account_name=None, account_key=None):
         self.name = name
+        # use default keys from account module
+        # but also allow to override that if needed
+        if not account_name and not account_key:
+            account_name = account.name
+            account_key = account.key
         self.queue_service = QueueService(
-            account_name=account.name,
-            account_key=account.key)
+            account_name, account_key)
 
     def get_queues(self):
+        "Return a list of queues"
+        return self.queue_service.list_queues()
+    
+    def get_queue_names(self):
         "Return a list of queue names"
-        queues_list = self.queue_service.list_queues()
+        queues_list = self.get_queues()
         queue_names_list = []
         for queue in queues_list:
             queue_names_list.append(queue.name)
         return queue_names_list
 
+    def queue_count(self):
+        "Return number of queues in this account"
+        return len(self.get_queues())
+
     def queue_exists(self, name):
         "Return True if the specified queue name exists"
-        if name in self.get_queues():
+        if name in self.get_queue_names():
             return True
         return False
 
