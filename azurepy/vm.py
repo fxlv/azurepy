0bb7e1b1ee997b94e9fc8a75ad383406ae69dc74
Kaspars Mickevics <kaspars@fx.lv>
2016-07-29 16:26:50 +0300

    Stylistic changes

--- azurepy/vm.py
+++ azurepy/vm.py
@@ -3,6 +3,7 @@ import json
 import os
 import sys
 
+
 class AzureVM():
     def __init__(self, subscription_id):
         self.subscription_id = subscription_id
@@ -15,7 +16,8 @@ class AzureVM():
         self.set_subscription()
 
     def set_subscription(self):
-        self.run_command("azure account set --json {}".format(self.subscription_id))
+        self.run_command("azure account set --json {}".format(
+            self.subscription_id))
 
     def run_command(self, command):
         print "DEBUG: running command: {}".format(command)
@@ -34,8 +36,11 @@ class AzureVM():
             print "DEBUG: JSON exception {}".format(e)
             print "DATA: {}".format(stdout)
             return False
+
     def delete_vm(vm_name, resource_group_name):
-        self.run_command("azure vm delete -n {} -g {}".format(vm_name, resource_group_name))
+        self.run_command("azure vm delete -n {} -g {}".format(
+            vm_name, resource_group_name))
+
     def check_subscription(self):
         """Return True if the provided subscription_id is valid."""
         accounts = self.get_accounts()
@@ -59,27 +64,35 @@ class AzureVM():
     def get_groups(self):
         groups = {}
         for group in self.run_command("azure group list --json"):
-            groups[group['name']] = {'id':group['id'], 'location':group['location']}
+            groups[group['name']] = {'id': group['id'],
+                                     'location': group['location']}
         return groups
 
     def get_group(self, group_name):
-        group = self.run_command("azure group show --json {}".format(group_name))
+        group = self.run_command("azure group show --json {}".format(
+            group_name))
         if not group:
             return False
         else:
             return group
+
     def resource_group_exists(self):
         return self.get_group(self.resource_group_name)
 
     def create_group(self, resource_group_name, resource_group_location):
-        self.run_command("azure group create --json -n {} -l {}".format(resource_group_name, resource_group_location))
+        self.run_command("azure group create --json -n {} -l {}".format(
+            resource_group_name, resource_group_location))
 
-    def create_vm(self, deployment_name, resource_group_name, resource_group_location, template_file_path, parameters_file_path):
+    def create_vm(self, deployment_name, resource_group_name,
+                  resource_group_location, template_file_path,
+                  parameters_file_path):
         self.resource_group_name = resource_group_name
         self.resource_group_location = resource_group_location
         # first check if requested resource group exists
         if not self.resource_group_exists():
             self.create_group(resource_group_name, resource_group_location)
         # create the VM itself
-        self.run_command("azure group deployment create --json -n {} -g {} -f {} -e {}".format(deployment_name, resource_group_name, template_file_path, parameters_file_path))
-
+        self.run_command(
+            "azure group deployment create --json -n {} -g {} -f {} -e {}".format(
+                deployment_name, resource_group_name, template_file_path,
+                parameters_file_path))
