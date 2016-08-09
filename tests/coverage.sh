ea57e8f15a822a435c83cb2fa65cd6d2f6cc802e
Kaspars Mickevics <kaspars@fx.lv>
2016-08-09 10:02:28 +0300

    One line for both modules

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 coverage erase
-coverage run --source azurepy/ -m pytest tests/test_queues.py
-coverage run --source azurepy/ -m pytest tests/test_vm.py
+coverage run --source azurepy/ -m pytest tests/test_queues.py tests/test_vm.py
 coverage html
 open htmlcov/index.html
